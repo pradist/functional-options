cdade800921e3338c95f2e18d39cfc146535bed7
Pradist Keawborisut <pradist.kea@ascendcorp.com>
2022-09-30 22:46:54 +0700

    feat: Declare a new constructor for each configuration option

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/pradist/functional/server"
 	"log"
+	"time"
 )
 
 func main() {
-	svr := server.New("localhost", 8080)
+	svr := server.NewWithTimeoutAndMaxConn("localhost", 1234, 30*time.Second, 10)
 	if err := svr.Start(); err != nil {
 		log.Fatal(err)
 	}
--- server/server.go
+++ server/server.go
@@ -1,17 +1,31 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Server struct {
-	host string
-	port int
+	host    string
+	port    int
+	timeout time.Duration
+	maxConn int
 }
 
 func New(host string, port int) *Server {
-	return &Server{host, port}
+	return &Server{host, port, time.Minute, 100}
+}
+
+func NewWithTimeout(host string, port int, timeout time.Duration) *Server {
+	return &Server{host: host, port: port, timeout: timeout}
+}
+
+func NewWithTimeoutAndMaxConn(host string, port int, timeout time.Duration, maxConn int) *Server {
+	return &Server{host, port, timeout, maxConn}
 }
 
 func (s *Server) Start() error {
 	fmt.Println("Starting server...")
+	fmt.Printf("%#v", s)
 	return nil
 }
