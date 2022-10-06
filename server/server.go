a128a548cd6343cf49b0d201cb48486dd05ba053
Pradist Keawborisut <pradist.kea@ascendcorp.com>
2022-10-06 22:56:12 +0700

    feat: Functional options pattern

--- server/server.go
+++ server/server.go
@@ -5,19 +5,47 @@ import (
 	"time"
 )
 
-type Server struct {
-	Cfg Config
-}
+var DefaultPort = 8080
 
-type Config struct {
+type Server struct {
 	Host    string
 	Port    int
 	Timeout time.Duration
 	MaxConn int
 }
 
-func New(cfg Config) *Server {
-	return &Server{Cfg: cfg}
+func New(options ...func(server *Server)) *Server {
+	s := &Server{
+		Port: DefaultPort,
+	}
+	for _, o := range options {
+		o(s)
+	}
+	return s
+}
+
+func WithHost(host string) func(*Server) {
+	return func(s *Server) {
+		s.Host = host
+	}
+}
+
+func WithPort(port int) func(*Server) {
+	return func(s *Server) {
+		s.Port = port
+	}
+}
+
+func WithTimeout(timeout time.Duration) func(*Server) {
+	return func(s *Server) {
+		s.Timeout = timeout
+	}
+}
+
+func WithMaxConn(maxConn int) func(*Server) {
+	return func(s *Server) {
+		s.MaxConn = maxConn
+	}
 }
 
 func (s *Server) Start() error {
